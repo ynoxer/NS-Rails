715f8918f833d33b8923f570ff7b7ca76aab9e0c
ynoxer <ynoxiuks@gmail.com>
2016-01-20 14:40:54 +0200

    Log-in-log-out

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106194440) do
+ActiveRecord::Schema.define(version: 20160107205501) do
 
   create_table "reservations", force: :cascade do |t|
     t.date     "from"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20160106194440) do
     t.string   "state"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "remember_digest"
   end
 
 end
